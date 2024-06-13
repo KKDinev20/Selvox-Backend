36e62d72a1a906c0856459d3e90b9736102c85c7
KKDinev20 <KKDinev20@codingburgas.bg>
2024-06-13 21:50:31 +0300

    Create Assessment Repository

---
+++ Selvox/Selvox.BLL/Repositories/Interfaces/IAssessmentRepository.cs
@@ -0,0 +1,11 @@
+﻿using Selvox.DAL.Models;
+
+namespace Selvox.BLL.Repositories.Interfaces;
+
+public interface IAssessmentRepository
+{
+    Task<Assessment> CreateAssessment(Assessment assessment);
+    Task<Assessment> GetAssessmentById(int id);
+    Task<IEnumerable<Assessment>> GetAssessmentByUserId(int userId);
+    Task<Assessment> UpdateAssessment(Assessment assessment);
+}
